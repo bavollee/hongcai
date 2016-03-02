ba62480f5a9aa988c8d4077e76e529dbae664b4b
hongcai <378139336@qq.com>
2016-03-02 13:47:56 +0800

    增加demo

---
+++ t1/Assets/Move.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+    public float m_speed = 1;
+    public bool touch = false;
+    public const float force = 40f;
+    bool run = false;
+    public float addSpVal = 0.01f;
+    float addSp;
+    float sp;
+    float round = 300f;
+    public Rigidbody r;
+    void Start()
+    {
+        r = GetComponent<Rigidbody>();
+    }
+    void OnCollisionEnter(Collision c)
+    {
+        Debug.Log("++++++++++++++OnCollisionEnter");
+        Move m = c.gameObject.GetComponent<Move>();
+        if (m == null)
+            return;
+
+        touch = true;
+        //c.gameObject.GetComponent<Move>().r.AddForce(getForce(force*0.5f).magnitude*r.velocity.normalized,ForceMode.Impulse);
+        //r.AddForce(getForce(force,false));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            
+        }
+        if (Input.GetKey(key))
+        {
+            run = true;
+        }
+        if (Input.GetKeyUp(key))
+        {
+            up();
+        }
+
+        if (run)
+        {
+            down();
+        }
+        else
+        {
+            gameObject.transform.Rotate(new Vector3(0, Time.deltaTime * sp, 0), Space.World);
+        }
+        //this.m_transform.Translate(new Vector3(moveh, 0, movev));
+    }
+    KeyCode key;
+    public void setKey(KeyCode c)
+    {
+        key = c;
+    }
+    public void setBtn(GameObject g)
+    {
+        UIEventListener.Get(g).onPress = onPress;
+    }
+    private void onPress(GameObject go, bool state)
+    {
+        if (state)
+        { 
+            run = true;
+        }
+        else
+        {
+            up();
+        }
+    }
+    
+    public void down()
+    {
+        sp = 0;
+        if (!touch)
+        {
+            r.AddForce(getForce());
+        }
+    }
+
+
+    public void up()
+    {
+        touch = false;
+        addSp = 0;
+        run = false;
+        round = -round;
+        sp = round;
+    }
+    public Vector3 getForce(float f = force,bool dir = true)
+    {
+        return transform.TransformDirection((dir?1 :-1)*Vector3.forward* f * r.mass * r.drag);
+    }
+
+
+    public void useMy()
+    {
+        if (addSp < 1)
+            addSp += addSpVal;
+        sp = 0;
+        transform.Translate(Vector3.forward * Time.deltaTime * 10f * addSp, Space.Self);
+    }
+}
+
+
+
+
+
+
+
+
